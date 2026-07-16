50e3d65b87d041145f456faec7d3fa9b348f3d55
Vinz <vinzpatel@gmail.com>
2026-07-16 15:05:17 +0800

    Add isolated v10 browser preview

---
+++ preview/sw.js
@@ -0,0 +1,30 @@
+const CACHE = "shay-zay-pixel-fun-v10";
+const CORE = ["./", "./index.html", "./manifest-comet.webmanifest", "./favicon.svg", "./comet-apple-touch-icon.png", "./comet-icon-192.png", "./comet-icon-512.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE);
+    await cache.addAll(CORE);
+    const indexResponse = await fetch("./index.html", { cache: "reload" });
+    const indexHtml = await indexResponse.text();
+    const builtAssets = [...indexHtml.matchAll(/(?:src|href)="([^"]+\.(?:js|css))"/g)].map((match) => match[1]);
+    if (builtAssets.length) await cache.addAll(builtAssets);
+    await self.skipWaiting();
+  })());
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET" || new URL(event.request.url).origin !== self.location.origin) return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+      if (response.ok) caches.open(CACHE).then((cache) => cache.put(event.request, response.clone()));
+      return response;
+    }).catch(() => event.request.mode === "navigate" ? caches.match("./index.html") : cached)),
+  );
+});
